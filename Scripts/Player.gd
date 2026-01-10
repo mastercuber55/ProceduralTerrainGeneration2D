034cd6dcfeb9426992e028e07f8664b061b9007a
Cube Nerd <mastercuber55@gmail.com>
2026-01-10 10:59:06 +0530

    Added plants and gave player a weapon cz why not

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,10 +1,26 @@
 extends CharacterBody2D
 
-const SPEED = 10000.0
+const SPEED := 100.0
 
+@onready var sprite := $PlayerSprite
+@onready var weapon := $Weapon
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	
-	velocity = Input.get_vector("MoveLeft", "MoveRight", "MoveUp", "MoveDown") * delta * SPEED
-
+	velocity = Input.get_vector("MoveLeft", "MoveRight", "MoveUp", "MoveDown") * SPEED
+	
+	if velocity.x < 0:
+		sprite.flip_h = true
+	elif velocity.x > 0:
+		sprite.flip_h = false
+		
+	if velocity.length() > 0:
+		var time = Time.get_ticks_msec() / 100
+		sprite.scale.y  = 1 + sin(time) / 10
+	else:
+		sprite.scale = Vector2.ONE
+		
+	if Input.is_action_just_pressed("Attack"):
+		$Weapon/AnimationPlayer.play("Swing")
+	
 	move_and_slide()
--- Scripts/ProceduralTerrainGenerator.gd
+++ Scripts/ProceduralTerrainGenerator.gd
@@ -1,19 +1,13 @@
-extends TileMapLayer
+extends Node2D
 
-const CHUNK_SIZE := 8
-const TILE_SIZE := 16
-const RENDER_DISTANCE := 3 # Chunks
-
-const TERRAIN_LEVELS := [
-	{ "max": -0.4, "tile": "Dirt",  "terrainID": 0 },
-	{ "max": 0.0,  "tile": "Plain Grass",   },
-	{ "max": 0.2,  "tile": "Grassy Grass",  },
-	{ "max": 1.0,  "tile": "Flowery Grass", }
-]
+@export var config: GenerationRules
 
 var noise := FastNoiseLite.new()
 var lookupTable := {}
 var chunks = {}
+
+@onready var ground := $"../Ground"
+@onready var plants := $"../Plants"
 
 func _ready() -> void:
 	
@@ -24,11 +18,11 @@ func _ready() -> void:
 	noise.frequency = 0.05
 
 func _process(_delta: float) -> void:
-	var playerPos = $"../Player".global_position
+	var playerPos = %Player.global_position
 	
 	var chunkCoords = Vector2i(
-		floor(playerPos.x / (CHUNK_SIZE * TILE_SIZE)),
-		floor(playerPos.y / (CHUNK_SIZE * TILE_SIZE))
+		floor(playerPos.x / (config.chunk_size * config.tile_size)),
+		floor(playerPos.y / (config.chunk_size * config.tile_size))
 	)
 	
 	for x in range(chunkCoords.x - 2, chunkCoords.x + 3):
@@ -39,19 +33,33 @@ func _process(_delta: float) -> void:
 				chunks[targetChunk] = true # marking as Active -> true
 				
 	for chunk in chunks.keys():
-		if 	abs(chunk.x - chunkCoords.x) > RENDER_DISTANCE or\
-			abs(chunk.y - chunkCoords.y) > RENDER_DISTANCE:
+		if 	abs(chunk.x - chunkCoords.x) > config.render_distance or\
+			abs(chunk.y - chunkCoords.y) > config.render_distance:
 				_remChunk(chunk)
 				chunks.erase(chunk)
 
+# TODO: make each plant be a dedicated scene with health and animation
+func _genPlant(n, pos):
+	
+	var rng := RandomNumberGenerator.new()
+	rng.seed = hash(pos) + n
+	var roll := rng.randf()
+	
+	for z in config.plant_chances:
+		if roll < z.chance:
+			plants.set_cell(pos, 0, z.tile)
+			return
+
 func _genChunk(chunk: Vector2i):	
-	var start := chunk * CHUNK_SIZE
+	var start = chunk * config.chunk_size
 	var groupTiles : Dictionary = {}
 	
-	for x in range(start.x, start.x + CHUNK_SIZE):
-		for y in range(start.y, start.y + CHUNK_SIZE):
+	for x in range(start.x, start.x + config.chunk_size):
+		for y in range(start.y, start.y + config.chunk_size):
 			var n = noise.get_noise_2d(x, y)
 			var data := _getTile(n)
+			
+			_genPlant(n, Vector2i(x, y))
 			
 			if data.has("terrainID"):
 				var terrainID = data["terrainID"]
@@ -60,38 +68,38 @@ func _genChunk(chunk: Vector2i):
 				groupTiles[terrainID].append(Vector2i(x, y))
 			else:
 				var lookupData = lookupTable[data.tile]
-				set_cell(Vector2i(x, y), lookupData.srcID, lookupData.coords)
+				ground.set_cell(Vector2i(x, y), lookupData.srcID, lookupData.coords)
 	
 	if groupTiles.is_empty():
 		return
 	
 	for group in groupTiles:
-		set_cells_terrain_connect(groupTiles[group], 0, group)
+		ground.set_cells_terrain_connect(groupTiles[group], 0, group)
 	
 func _remChunk(chunk: Vector2i):	
-	var start = chunk * CHUNK_SIZE
+	var start = chunk * config.chunk_size
 	
-	for x in range(start.x, start.x + CHUNK_SIZE):
-		for y in range(start.y, start.y + CHUNK_SIZE):
-			set_cell(Vector2i(x, y), -1)
+	for x in range(start.x, start.x + config.chunk_size):
+		for y in range(start.y, start.y + config.chunk_size):
+			ground.set_cell(Vector2i(x, y), -1)
 			
 func _getTile(n: float) -> Dictionary:
-	for level in TERRAIN_LEVELS:
+	for level in config.terrain_levels:
 		if n <= level.max:
 			return level
-	return TERRAIN_LEVELS[0]
+	return config.terrain_levels[0]
 
 func _buildTileLookUp():
 	
-	var srcCount := tile_set.get_source_count()
+	var srcCount : int = ground.tile_set.get_source_count()
 	
 	for iSrc in range(srcCount):
-		var srcID := tile_set.get_source_id(iSrc)
-		var src = tile_set.get_source(srcID)
+		var srcID = ground.tile_set.get_source_id(iSrc)
+		var src = ground.tile_set.get_source(srcID)
 		
 		if src is TileSetAtlasSource:
 			for iTiles in range(src.get_tiles_count()):
-				var coords := src.get_tile_id(iTiles)	
+				var coords = src.get_tile_id(iTiles)	
 				var tileData = src.get_tile_data(coords, 0)
 				
 				if tileData == null:
