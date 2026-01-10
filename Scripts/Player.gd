fd309897e3fd0e388c17197bcb34be2200f7b551
Cube Nerd <mastercuber55@gmail.com>
2026-01-10 12:35:12 +0530

    Fixed player scale inversion

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,25 +2,25 @@ extends CharacterBody2D
 
 const SPEED := 100.0
 
-@onready var sprite := $PlayerSprite
-@onready var weapon := $Weapon
+@onready var sprite := $Pivot/PlayerSprite
+@onready var weapon := $Pivot/Weapon
 
 func _physics_process(_delta: float) -> void:
 	
 	velocity = Input.get_vector("MoveLeft", "MoveRight", "MoveUp", "MoveDown") * SPEED
 	
 	if velocity.x < 0:
-		sprite.flip_h = true
+		$Pivot.scale.x = -1
 	elif velocity.x > 0:
-		sprite.flip_h = false
+		$Pivot.scale.x = 1
 		
 	if velocity.length() > 0:
-		var time = Time.get_ticks_msec() / 100
+		var time = Time.get_ticks_msec() * 0.01
 		sprite.scale.y  = 1 + sin(time) / 10
 	else:
-		sprite.scale = Vector2.ONE
+		sprite.scale.y = 1
 		
 	if Input.is_action_just_pressed("Attack"):
-		$Weapon/AnimationPlayer.play("Swing")
+		weapon.get_node("AnimationPlayer").play("Swing")
 	
 	move_and_slide()
